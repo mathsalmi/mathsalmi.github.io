9387906c830c30afe2642ec11a3fab560f844672
Matheus Salmi <mathsalmi@gmail.com>
2019-11-25 10:27:12 -0300

    Change build script

---
+++ stocks/precache-manifest.f6400710e84fdcbbb0905ecd989314e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80e21be8c407df32815ab7504f702c5a",
+    "url": "/stocks/index.html"
+  },
+  {
+    "revision": "8c36046e24fb2ce2e5fb",
+    "url": "/stocks/static/css/2.043a084f.chunk.css"
+  },
+  {
+    "revision": "e9e3044a52dfa1a4f2b5",
+    "url": "/stocks/static/css/main.88e81125.chunk.css"
+  },
+  {
+    "revision": "8c36046e24fb2ce2e5fb",
+    "url": "/stocks/static/js/2.971047fd.chunk.js"
+  },
+  {
+    "revision": "e9e3044a52dfa1a4f2b5",
+    "url": "/stocks/static/js/main.52b11798.chunk.js"
+  },
+  {
+    "revision": "3a4befa892017606b698",
+    "url": "/stocks/static/js/runtime-main.e7a6981b.js"
+  }
+]);
